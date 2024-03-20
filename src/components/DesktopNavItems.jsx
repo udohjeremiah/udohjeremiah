37e68f2187c48ce542da2fda42b66cce65cf229f
Udoh Jeremiah <udohjeremiah@icloud.com>
2024-03-20 12:19:04 +0100

    Update: website photos and some css layout restyling

--- src/components/DesktopNavItems.jsx
+++ src/components/DesktopNavItems.jsx
@@ -5,6 +5,11 @@ import Link from "next/link";
 import { ExternalLinkIcon } from "@radix-ui/react-icons";
 
 // Components
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import ThemeToggler from "./ThemeToggler";
 
 // Data
@@ -13,61 +18,108 @@ import { sections } from "@/data/navigation";
 // Lib
 import { cn } from "@/lib/utils";
 
-export default function DesktopNavItems({ pathname }) {
+export default function DesktopNavItems({ pathname, isDesktopNavOpened }) {
   return (
     <nav>
-      {sections.map((section, sectionIndex) => (
-        <section key={sectionIndex} className="space-y-2 p-3">
-          {section.name ? (
+      {isDesktopNavOpened ? (
+        <>
+          {sections.map((section, sectionIndex) => (
+            <section key={sectionIndex} className="space-y-2 p-3">
+              {section.name ? (
+                <h3 className="mx-3 text-xs font-semibold uppercase tracking-wide">
+                  {section.name}
+                </h3>
+              ) : null}
+              <div className="space-y-0.5">
+                {section.links.map(
+                  ({ name, href, icon: Icon, shortcut, active }) => (
+                    <Link
+                      href={href}
+                      key={name}
+                      target={href.startsWith("http") ? "_blank" : undefined}
+                      rel={
+                        href.startsWith("http")
+                          ? "noopener noreferrer"
+                          : undefined
+                      }
+                      className={cn(
+                        "group flex items-center justify-center gap-2.5 rounded-md border px-3 py-2 transition-colors",
+                        active?.(pathname)
+                          ? "bg-primary-foreground"
+                          : "border-transparent text-muted-foreground hover:bg-primary-foreground hover:text-foreground",
+                      )}
+                    >
+                      {Icon && <Icon className="h-4 w-4 shrink-0" />}
+                      <p className="flex-1 text-sm font-medium leading-[22px]">
+                        {name}
+                      </p>
+                      {shortcut ? (
+                        <kbd className="flex h-5 w-4 shrink-0 items-center justify-center rounded border text-[10px] font-medium uppercase text-muted-foreground">
+                          {shortcut}
+                        </kbd>
+                      ) : null}
+                      {["http", "mailto:"].some((prefix) =>
+                        href.startsWith(prefix),
+                      ) ? (
+                        <ExternalLinkIcon className="h-4 w-4 shrink-0 text-muted-foreground transition-colors" />
+                      ) : null}
+                    </Link>
+                  ),
+                )}
+              </div>
+            </section>
+          ))}
+          <section className="space-y-2 p-3">
             <h3 className="mx-3 text-xs font-semibold uppercase tracking-wide">
-              {section.name}
+              Other
             </h3>
-          ) : null}
-          <div className="space-y-0.5">
+            <div className="space-y-0.5">
+              <ThemeToggler />
+            </div>
+          </section>
+        </>
+      ) : (
+        sections.map((section, sectionIndex) => (
+          <section key={sectionIndex} className="px-1 py-3">
             {section.links.map(
               ({ name, href, icon: Icon, shortcut, active }) => (
-                <Link
-                  href={href}
-                  key={name}
-                  target={href.startsWith("http") ? "_blank" : undefined}
-                  rel={
-                    href.startsWith("http") ? "noopener noreferrer" : undefined
-                  }
-                  className={cn(
-                    "group flex items-center gap-2.5 rounded-md border px-3 py-2 transition-colors",
-                    active?.(pathname)
-                      ? "bg-primary-foreground"
-                      : "border-transparent text-muted-foreground hover:bg-primary-foreground hover:text-foreground",
-                  )}
-                >
-                  {Icon ? <Icon className="h-4 w-4 shrink-0" /> : null}
-                  <p className="flex-1 text-sm font-medium leading-[22px]">
-                    {name}
-                  </p>
-                  {shortcut ? (
-                    <kbd className="flex h-5 w-4 shrink-0 items-center justify-center rounded border text-[10px] font-medium uppercase text-muted-foreground">
-                      {shortcut}
-                    </kbd>
-                  ) : null}
-                  {["http", "mailto:"].some((prefix) =>
-                    href.startsWith(prefix),
-                  ) ? (
-                    <ExternalLinkIcon className="h-4 w-4 shrink-0 text-muted-foreground transition-colors" />
-                  ) : null}
-                </Link>
+                <Tooltip key={sectionIndex} delayDuration={0}>
+                  <TooltipTrigger asChild>
+                    <Link
+                      href={href}
+                      key={name}
+                      target={href.startsWith("http") ? "_blank" : undefined}
+                      rel={
+                        href.startsWith("http")
+                          ? "noopener noreferrer"
+                          : undefined
+                      }
+                      className={cn(
+                        "group flex items-center justify-center gap-2.5 rounded-md border px-3 py-2 transition-colors",
+                        active?.(pathname)
+                          ? "bg-primary-foreground"
+                          : "border-transparent text-muted-foreground hover:bg-primary-foreground hover:text-foreground",
+                      )}
+                    >
+                      {Icon && <Icon className="h-4 w-4 shrink-0" />}
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent side="right" className="flex items-center">
+                    <div className="flex items-center">
+                      <p>{name}</p>
+                      {["http", "mailto:"].some((prefix) =>
+                        href.startsWith(prefix),
+                      ) ? (
+                        <ExternalLinkIcon className="ml-2 h-4 w-4 shrink-0 text-muted-foreground transition-colors" />
+                      ) : null}
+                    </div>
+                  </TooltipContent>
+                </Tooltip>
               ),
             )}
-          </div>
-        </section>
-      ))}
-      <section className="space-y-2 p-3">
-        <h3 className="mx-3 text-xs font-semibold uppercase tracking-wide">
-          Other
-        </h3>
-        <div className="space-y-0.5">
-          <ThemeToggler />
-        </div>
-      </section>
+          </section>
+        ))
+      )}
     </nav>
   );
 }
