b146a0dc28731e3fd2dd5bacebf41badc410cdcc
Udoh Jeremiah <udohjeremiah@icloud.com>
2024-10-02 14:58:14 +0100

    Add /readings route.

---
+++ src/app/(pages)/readings/page.jsx
@@ -0,0 +1,81 @@
+import Image from "next/image";
+
+import Card from "@/components/Card";
+import Header from "@/components/Header";
+import Link from "@/components/Link";
+
+import readings from "@/data/readings.json";
+
+import { tw } from "@/lib/utils";
+
+const title = "Readings";
+const description = "Materials I've read since launching this site.";
+
+export const metadata = {
+  title,
+  description,
+};
+
+function Book({ data }) {
+  return (
+    <Link
+      key={data.href}
+      href={data.href}
+      label={data.name}
+      className={tw(
+        "flex flex-col gap-4 rounded-lg p-4 no-underline transition-colors",
+        "hover:bg-neutral-100",
+        "dark:hover:bg-neutral-800",
+      )}
+    >
+      <Image
+        src={data.image}
+        alt={data.name}
+        width={128}
+        height={196}
+        quality={100}
+        className="rounded-md"
+      />
+      <div>
+        <p
+          className={tw(
+            "text-sm font-medium",
+            "text-neutral-900",
+            "dark:text-neutral-100",
+          )}
+        >
+          {data.name}
+        </p>
+        <p
+          className={tw("text-sm", "text-neutral-500", "dark:text-neutral-400")}
+        >
+          {data.author}
+        </p>
+      </div>
+    </Link>
+  );
+}
+
+export default function ReadingsPage() {
+  return (
+    <>
+      <Header title={title} description={description} />
+      <div className="not-prose mt-8 grid gap-8">
+        {Object.values(readings).map(({ category, books }) => (
+          <Card
+            key={category}
+            title={category}
+            className={tw(
+              "grid grid-cols-2 gap-2 p-2",
+              "md:grid-cols-3 lg:grid-cols-4",
+            )}
+          >
+            {books.map((book) => (
+              <Book data={book} key={book.name} />
+            ))}
+          </Card>
+        ))}
+      </div>
+    </>
+  );
+}
