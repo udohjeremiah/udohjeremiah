28b19826fcbf3574e9bbdb53ba783e349e7009a1
Udoh Jeremiah <udohjeremiah@icloud.com>
2024-10-02 14:42:28 +0100

    Add code copy functionality to blog posts.

---
+++ src/components/Pre.jsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+import CheckIcon from "@/components/icons/CheckIcon";
+import CopyIcon from "@/components/icons/CopyIcon";
+
+export default function Pre({ children, ...props }) {
+  const [showCopy, setShowCopy] = useState(false);
+  const [isCopied, setIsCopied] = useState(false);
+  const preRef = useRef(null);
+
+  useEffect(() => {
+    const dataLanguage = preRef.current.hasAttribute("data-language");
+
+    if (dataLanguage) {
+      setShowCopy(true);
+    }
+  }, []);
+
+  const handleCopy = async () => {
+    const code = preRef.current?.textContent;
+
+    if (code) {
+      await navigator.clipboard.writeText(code);
+      setIsCopied(true);
+
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 3000);
+    }
+  };
+
+  return (
+    <pre ref={preRef} {...props}>
+      {showCopy && (
+        <button
+          aria-label={isCopied ? "Copied" : "Copy to clipboard"}
+          disabled={isCopied}
+          onClick={handleCopy}
+          className="absolute right-4 top-2 h-6 w-6"
+        >
+          {isCopied ? (
+            <CheckIcon className="h-5 w-5 text-green-500" />
+          ) : (
+            <CopyIcon className="h-5 w-5" />
+          )}
+        </button>
+      )}
+      {children}
+    </pre>
+  );
+}
