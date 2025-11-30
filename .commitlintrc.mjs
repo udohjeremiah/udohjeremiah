37ab35f376744d786b52459be1ff296f07d7b470
Udoh Jeremiah <udohjeremiah@icloud.com>
2025-11-30 23:38:17 +0100

    feat: use default commitlint config
    This removes unnecessary rules and relies on extending only the default conventional commit

--- .commitlintrc.mjs
+++ .commitlintrc.mjs
@@ -1,47 +1,4 @@
-import { RuleConfigSeverity } from "@commitlint/types";
-
 /** @type {import("@commitlint/types").UserConfig} */
 export default {
   extends: ["@commitlint/config-conventional"],
-  rules: {
-    // Body and footer can be multiline and lengthy (allow for detailed info)
-    "body-max-line-length": [RuleConfigSeverity.Disabled, "always", Infinity],
-    "footer-max-line-length": [RuleConfigSeverity.Disabled, "always", Infinity],
-
-    // Header length must be concise and readable
-    "header-max-length": [RuleConfigSeverity.Error, "always", 72],
-
-    // Allow references (e.g. issues, PRs); allow empty reference field (relaxed)
-    "references-empty": [RuleConfigSeverity.Disabled],
-
-    // Scope must not be empty (encourages more context)
-    "scope-empty": [RuleConfigSeverity.Error, "never"],
-
-    // Type must not be empty
-    "type-empty": [RuleConfigSeverity.Error, "never"],
-
-    // Enforce commit types
-    "type-enum": [
-      RuleConfigSeverity.Error,
-      "always",
-      [
-        "build", // build system changes
-        "chore", // maintenance tasks
-        "ci", // CI/CD changes
-        "docs", // documentation only
-        "feat", // new feature
-        "fix", // bug fix
-        "note", // adding or updating a note
-        "perf", // performance improvements
-        "post", // adding or updating a blog post
-        "refactor", // refactoring code, no feature or fix
-        "revert", // reverting changes
-        "style", // formatting, missing semi colons, etc.
-        "test", // adding or correcting tests
-      ],
-    ],
-
-    // Enforce lower case type (fix, feat, chore, etc.)
-    "type-case": [RuleConfigSeverity.Error, "always", "lower-case"],
-  },
 };
